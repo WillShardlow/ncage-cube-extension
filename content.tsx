93fc67359c703185981ca55ce5d91312b1ea18d3
Will Shardlow <Will.Shardlow@ghyston.com>
2023-10-30 10:01:14 +0000

    initial commit

---
+++ content.tsx
@@ -0,0 +1,11 @@
+import {NCage} from "~node_modules/ncage-cube";
+
+function Content() {
+    return (
+        <div>
+            <NCage alwaysVisible={true}/>
+        </div>
+    )
+}
+
+export default Content
